02ec0a399e4d584b8492cce41c20311acc55d51a
dldnjsrn <dldnjsrn21933@naver.comgit config --listgit init git config --global user.email dldnjsrn21933@naver.com>
2024-11-12 17:22:55 +0900

    3차업데이트

---
+++ js/sub-script-3.js
@@ -0,0 +1,3 @@
+$(function () {
+    gsap.registerPlugin(ScrollTrigger);
+});
--- js/sub-script2.js
+++ js/sub-script2.js
@@ -1,24 +1,42 @@
-$(function () {
-    var productsSwiper = new Swiper(".products-swiper", {
-        slidesPerView: 3.5,
-        spaceBetween: 20,
-        // centeredSlides: true,
-        // freeMode: true,
-        // pauseOnHover: true,
+const productsSwiper = new Swiper(".products-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    // centeredSlides: true,
+    // freeMode: true,
+    // pauseOnHover: true,
 
-        breakpoints: {
-            1024: { slidesPerView: 2.5 },
+    breakpoints: {
+        400: {
+            slidesPerView: 1.3,
+            spaceBetween: 30,
         },
+        1024: { slidesPerView: 3.5, spaceBetween: 30 },
+    },
 
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            type: "progressbar",
-        },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "progressbar",
+    },
 
-        navigation: {
-            nextEl: ".button-next",
-            prevEl: ".button-prev",
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    },
+});
+
+var productsSwiper2 = new Swiper(".products-swiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    breakpoints: {
+        400: {
+            slidesPerView: 2.2,
+            spaceBetween: 30,
         },
-    });
+    },
+
+    pagination: {
+        clickable: true,
+    },
 });
